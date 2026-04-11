115684e1a735e5069c80d4aec618b948e9da6af8
shravanxd <ssk10036@nyu.edu>
2026-04-10 21:02:11 -0400

    security: strict CORS policy and patch next.js server components DoS vulnerability

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,8 +18,8 @@ app = FastAPI(
 if settings.BACKEND_CORS_ORIGINS:
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],
-        allow_credentials=False,
+        allow_origins=settings.BACKEND_CORS_ORIGINS,
+        allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
     )
